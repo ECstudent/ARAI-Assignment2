a0bbbbec357ca822bb1ba6348899338f706ac60e
ran510 <ran510@PC-011260.vu.local>
2015-05-01 17:20:40 +0200

    Finally finished.
    The first puzzle takes about 18 seconds to solve, seems somewhat slow.
    Might just be due to the university pc hardware.

--- SudokuCSP/src/arai/csp/CSPSolver.java
+++ SudokuCSP/src/arai/csp/CSPSolver.java
@@ -5,10 +5,19 @@ import java.util.Map;
 
 public class CSPSolver {
 
+	public static final int NUMBER_OF_BOXES_IN_ROW = 9;
+	public static final int NUMBER_OF_BOXES_IN_CLM = 9;
+	public static final int NUMBER_OF_BOXES_IN_REG = 9;
+	public static final int NUMBER_OF_REGIONS_IN_ROW = 3;
+	public static final int NUMBER_OF_REGIONS_IN_CLM = 3;
 	public static final int NUMBER_OF_BOXES = 81;
 
-	// True if the puzzle has been solved
-	private boolean solved = false;
+	public static final int ROW = 0;
+	public static final int COLUMN = 1;
+	public static final int REGION = 2;
+
+	// Used for displaying the puzzle as it is being solved.
+	private long printDelay = 0;
 
 	// A String array containing the domains for all variables
 	private String[] variables;
@@ -21,8 +30,11 @@ public class CSPSolver {
 	private final int[] MULTIPLE_OF_NINE = new int[] { 9, 18, 27, 36, 45, 54,
 			63, 72, 81 };
 
-	private Map<Integer, Integer> locRow;
-	private Map<Integer, Integer> locClm;
+	// Holds the coordinates of all variables
+	// The value is formatted as "rowcolumnregion", e.g., "142" would be the
+	// first row, third column and second region
+	// Note: Region numbers are calculated left-to-right, top-to-bottom.
+	private Map<Integer, String> locs;
 
 	/**
 	 * Initialize the variables and domains.
@@ -31,19 +43,35 @@ public class CSPSolver {
 	 */
 	private void initVariables() {
 		variables = new String[NUMBER_OF_BOXES];
-		locRow = new HashMap<Integer, Integer>();
-		locClm = new HashMap<Integer, Integer>();
+		locs = new HashMap<Integer, String>();
 
-		for (int i = 0; i < NUMBER_OF_BOXES; i++) {
-			if (puzzle.charAt(i) == '.') {
-				variables[i] = "123456789";
+		int indexRow = 0;
+		int indexClm = 1;
+		int indexReg = 0;
+		int regCount = -NUMBER_OF_REGIONS_IN_ROW;
+
+		for (int index = 0; index < NUMBER_OF_BOXES; index++, indexClm++) {
+			if (puzzle.charAt(index) == '.') {
+				variables[index] = "123456789";
 			} else {
-				variables[i] = "" + puzzle.charAt(i);
+				variables[index] = "" + puzzle.charAt(index);
 			}
-			String loc = computeLoc(i);
-			locRow.put(i, Integer.valueOf(loc.split(",")[0]));
-			locClm.put(i, Integer.valueOf(loc.split(",")[1]));
+
+			if (index % NUMBER_OF_REGIONS_IN_ROW == 0) {
+				indexReg++;
+				if (index % NUMBER_OF_BOXES_IN_ROW == 0) {
+					indexRow++;
+					indexClm = 1;
+					if (index
+							% (NUMBER_OF_BOXES_IN_ROW * NUMBER_OF_REGIONS_IN_ROW) == 0)
+						regCount += NUMBER_OF_REGIONS_IN_ROW;
+					indexReg = 1 + regCount;
+				}
+			}
+			locs.put(index, indexRow + "" + indexClm + "" + indexReg);
 		}
+
+		System.out.println(solution());
 	}
 
 	/**
@@ -54,6 +82,7 @@ public class CSPSolver {
 	 * @return A String containing the row and column of the variable, separated
 	 *         by a comma.
 	 */
+	@Deprecated
 	private String computeLoc(int index) {
 		int column = 0;
 		int row = 0;
@@ -73,65 +102,58 @@ public class CSPSolver {
 	}
 
 	/**
-	 * Check the row of the given variable to reduce its domain.
+	 * Check the row, column or region of the given variable to reduce its
+	 * domain.
 	 * 
 	 * If an element of the given variable is already present in another
-	 * variable with domain length 1 (assigned) in the same row, then remove
-	 * that element from its domain.
+	 * variable with domain length 1 (assigned) in the same row, column or
+	 * region, then remove that element from its domain.
 	 * 
 	 * Most basic reasoning method for solving Sudoku puzzles. May not be
 	 * sufficient for more difficult puzzles.
 	 * 
+	 * @param type
+	 *            The area in the puzzle to check its constraints for. Can be
+	 *            either ROW, COLUMN or REGION (0, 1 or 2).
 	 * @param index
 	 *            The index of the current variable in the variables array.
+	 * @param tempVariables
 	 */
-	private void constraintRow(int index) {
-		int row = locRow.get(index);
-		for (int boxInRow = 0; boxInRow < locRow.size(); boxInRow++) {
-			if (locRow.get(boxInRow) == row && index != boxInRow) {
-				for (String value : variables[index].split("")) {
-					if (variables[boxInRow].length() == 1
-							&& variables[boxInRow].contains(value)) {
-						variables[index].replace(value, "");
-					}
-				}
+	private void basicConstraints(int type, int index, String[] tempVariables) {
+		int loc = Integer.valueOf(locs.get(index).split("(?!^)")[type]);
+
+		// Check the actual constraints...
+		// If a possible value in the variables domain has already been
+		// assigned to another variable in the same ROW, COLUMN or REGION,
+		// then it can't possibly be assigned to the current variable and
+		// must, therefore, be removed from its domain.
+		for (String value : tempVariables[index].split("(?!^)")) {
+			for (int box = 0; box < NUMBER_OF_BOXES; box++) {
+				int boxLoc = Integer
+						.valueOf(locs.get(box).split("(?!^)")[type]);
+
+				// Ignore itself and other boxes not in the same ROW, COLUMN or
+				// REGION.
+				if (boxLoc != loc || index == box)
+					continue;
+
+				// We only need to check with boxes that have assigned values.
+				if (tempVariables[box].length() != 1)
+					continue;
+
+				// Remove the value if it has already been assigned to another
+				// variable.
+				if (tempVariables[box].contains(value))
+					tempVariables[index] = tempVariables[index].replace(value,
+							"");
 			}
 		}
-	}
-
-	/**
-	 * Check the column of the given variable to reduce its domain.
-	 * 
-	 * If an element of the given variable is already present in another
-	 * variable with domain length 1 (assigned) in the same column, then remove
-	 * that element from its domain.
-	 * 
-	 * Most basic reasoning method for solving Sudoku puzzles. May not be
-	 * sufficient for more difficult puzzles.
-	 * 
-	 * @param index
-	 *            The index of the current variable in the variables array.
-	 */
-	private void constraintColumn(int index) {
-		int clm = locClm.get(index);
-		for (int boxInClm = 0; boxInClm < locClm.size(); boxInClm++) {
-			if (locClm.get(boxInClm) == clm && index != boxInClm) {
-				for (String value : variables[index].split("")) {
-					if (variables[boxInClm].length() == 1
-							&& variables[boxInClm].contains(value)) {
-						variables[index].replace(value, "");
-					}
-				}
-			}
-		}
-	}
-
-	private void constraintRegion(int index) {
-
 	}
 
 	/**
 	 * Used to find a solution for a Sudoku puzzle using a CSP approach.
+	 * 
+	 * Includes initialization of variables and domains.
 	 * 
 	 * @param puzzle
 	 *            The Sudoku puzzle to be solved.
@@ -141,12 +163,141 @@ public class CSPSolver {
 
 		initVariables();
 
-		reduceDomains();
+		printDelay = System.currentTimeMillis();
 
-		if (!isSolved()) {
-			// heuristic to assign value to variable
-			// e.g. first variables with smallest domains
+		dfSearch(variables.clone());
+
+		System.out.println(solution());
+	}
+
+	/**
+	 * Use a depth-first, recursive approach to solve the puzzle.
+	 * 
+	 * At every level constraint propagation takes place as far as possible
+	 * (this can be further extended upon as described in constraintProp()).
+	 * 
+	 * After constraint propagation, we need to check if an empty domain is
+	 * present for any of the variables. This indicates that (at least) one of
+	 * the previous assigned variables was incorrect.
+	 * 
+	 * If there is no empty domain found (yet), we need to check if the puzzle
+	 * is solved (all variables have correct assignments). If it is solved, copy
+	 * the final assignments to the original variable array and end the search.
+	 * 
+	 * If it is not (yet) solved, use a heuristic to determine which variable
+	 * needs to be assigned a value first. An ordered list is created to loop
+	 * over (see method varSelection()). Whenever an assigned variable leads to
+	 * an empty domain (i.e., it is incorrect), try the next value, etc. If all
+	 * of the possible variable assignments lead to an empty domain, go "up" one
+	 * level and try the next variable value there, etc.
+	 * 
+	 * @param tempVariables
+	 * @return
+	 */
+	private boolean dfSearch(String[] tempVariables) {
+
+		constraintProp(tempVariables);
+
+		if (hasEmptyDomain(tempVariables))
+			return false;
+
+		if (!isSolved(tempVariables)) {
+			int varIndex = singleVarSelection(tempVariables);
+			for (String value : tempVariables[varIndex].split("(?!^)")) {
+				tempVariables[varIndex] = value;
+				if (printDelay + 100 < System.currentTimeMillis()) {
+					System.out.println(getCurrentAssignments(tempVariables));
+					printDelay = System.currentTimeMillis();
+				}
+				if (dfSearch(tempVariables.clone()))
+					return true;
+			}
+			return false;
+		} else {
+			variables = tempVariables.clone();
 		}
+
+		return true;
+	}
+
+	/**
+	 * Heuristic used for variable selection.
+	 * 
+	 * The variable with the shortest domain is selected for assignment.
+	 * Elements in the variables domain are chosen left-to-right (no heuristic).
+	 * 
+	 * @param tempVariables
+	 * @return
+	 */
+	private int singleVarSelection(String[] tempVariables) {
+
+		int varIndex = 0;
+
+		int shortest = Integer.MAX_VALUE;
+
+		for (int i = 0; i < NUMBER_OF_BOXES; i++) {
+			if (tempVariables[i].length() > 1
+					&& tempVariables[i].length() < shortest) {
+				shortest = tempVariables[i].length();
+				varIndex = i;
+			}
+		}
+
+		return varIndex;
+	}
+
+	/**
+	 * Heuristic used for variable selection.
+	 * 
+	 * All variables are ordered by the length of their domain, short-to-long.
+	 * Elements in the variables domain are chosen left-to-right (no heuristic).
+	 * 
+	 * @param tempVariables
+	 * @return
+	 */
+	@Deprecated
+	private int[] varSelection(String[] tempVariables) {
+
+		int varLength = 0;
+		int varIndex = 0;
+		int tempVarLength = 0;
+		int tempVarIndex = 0;
+
+		int[] lengthSort = new int[NUMBER_OF_BOXES];
+		int[] indexSort = new int[NUMBER_OF_BOXES];
+
+		// 16 was chosen because it is the smallest value larger than the
+		// maximum domain length that requires a single bit-flip
+		for (int i = 0; i < NUMBER_OF_BOXES; i++) {
+			lengthSort[i] = 16;
+			indexSort[i] = 0;
+		}
+
+		for (int i = 0; i < NUMBER_OF_BOXES; i++) {
+			varLength = tempVariables[i].length();
+
+			if (varLength < 2)
+				continue;
+
+			varIndex = i;
+			for (int index = 0; index < NUMBER_OF_BOXES; index++) {
+				if (varLength < lengthSort[index]) {
+					// sorting by length of domain is used...
+					tempVarLength = lengthSort[index];
+					lengthSort[index] = varLength;
+					varLength = tempVarLength;
+					// ...to sort index of variables
+					tempVarIndex = indexSort[index];
+					indexSort[index] = varIndex;
+					varIndex = tempVarIndex;
+				}
+				// stop current loop when the end of the valid values is reached
+				if (varLength == 16)
+					break;
+			}
+		}
+
+		return indexSort;
 	}
 
 	/**
@@ -155,37 +306,37 @@ public class CSPSolver {
 	 * that the puzzle has been solved or that more advanced solving techniques
 	 * are necessary to complete it.
 	 */
-	private void reduceDomains() {
+	private void constraintProp(String[] tempVariables) {
 		boolean reduced = false;
 
 		for (int index = 0; index < NUMBER_OF_BOXES; index++) {
-			if (variables[index].length() == 1)
+			if (tempVariables[index].length() == 1)
 				continue;
 
-			int var_length = variables[index].length();
+			int varLength = tempVariables[index].length();
 
-			constraintRow(index);
-			constraintColumn(index);
-			constraintRegion(index);
-			// more constraints...
+			basicConstraints(ROW, index, tempVariables);
+			basicConstraints(COLUMN, index, tempVariables);
+			basicConstraints(REGION, index, tempVariables);
+			// more (advanced) constraints...
 
-			if (variables[index].length() < var_length)
+			if (!reduced && tempVariables[index].length() < varLength)
 				reduced = true;
 		}
 
-		if (reduced) {
-			reduceDomains();
-		}
+		if (reduced)
+			constraintProp(tempVariables);
 	}
 
 	/**
 	 * Check if the puzzle has been fully solved. A puzzle has been solved if
 	 * for all variables the domain has been reduced to a length of 1.
 	 * 
+	 * @param tempVariables
 	 * @return
 	 */
-	public boolean isSolved() {
-		for (String var : variables) {
+	public boolean isSolved(String[] tempVariables) {
+		for (String var : tempVariables) {
 			if (var.length() == 1)
 				continue;
 			return false;
@@ -194,19 +345,85 @@ public class CSPSolver {
 	}
 
 	/**
-	 * Retrieve the solution to the given puzzle, if any.
+	 * Check if the current assignment contains a variable with no possible
+	 * assignments (an empty domain).
+	 * 
+	 * @param tempVariables
+	 * @return
+	 */
+	public boolean hasEmptyDomain(String[] tempVariables) {
+		for (String var : tempVariables) {
+			if (!var.isEmpty())
+				continue;
+			return true;
+		}
+		return false;
+	}
+
+	/**
+	 * Retrieve the solution to the current puzzle, if any.
 	 * 
 	 * @return
 	 */
 	public String solution() {
-		if (isSolved()) {
-			String solution = "";
-			for (String s : variables) {
-				solution += s;
-			}
-			return solution;
+		String solution = "";
+
+		if (isSolved(variables)) {
+			solution += "Solution is shown below.\n";
 		} else {
-			return null;
+			solution += "Not (yet) solved. See original puzzle below.\n";
 		}
+
+		for (int index = 0; index < NUMBER_OF_BOXES; index++) {
+			if (index == 0) {
+				solution += variables[index].length() > 1 ? "?"
+						: variables[index].length() < 1 ? "."
+								: variables[index];
+				continue;
+			}
+			if (index % NUMBER_OF_REGIONS_IN_ROW == 0) {
+				solution += "|";
+				if (index % NUMBER_OF_BOXES_IN_ROW == 0) {
+					solution += "\n";
+					if (index
+							% (NUMBER_OF_BOXES_IN_ROW * NUMBER_OF_REGIONS_IN_ROW) == 0) {
+						solution += "--- --- ---\n";
+					}
+				}
+			}
+			solution += variables[index].length() > 1 ? "?" : variables[index]
+					.length() < 1 ? "." : variables[index];
+		}
+
+		return solution + "|\n";
+	}
+
+	/**
+	 * Retrieve the solution to the current puzzle, if any.
+	 * 
+	 * @return
+	 */
+	public String getCurrentAssignments(String[] tempVariables) {
+		String solution = "Current Assignments...\n";
+
+		for (int index = 0; index < NUMBER_OF_BOXES; index++) {
+			if (index == 0) {
+				solution += tempVariables[index];
+				continue;
+			}
+			if (index % NUMBER_OF_REGIONS_IN_ROW == 0) {
+				solution += "|";
+				if (index % NUMBER_OF_BOXES_IN_ROW == 0) {
+					solution += "\n";
+					if (index
+							% (NUMBER_OF_BOXES_IN_ROW * NUMBER_OF_REGIONS_IN_ROW) == 0) {
+						solution += "--- --- ---\n";
+					}
+				}
+			}
+			solution += tempVariables[index];
+		}
+
+		return solution + "|\n";
 	}
 }
